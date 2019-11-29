f4223e83b8831675bb32d489b5155b58df05e918
Vincent <courtois.vincent@outlook.com>
2019-11-29 10:01:25 +0100

    Add missing documentation

--- lib/virtuatable/builders/helpers/loaders.rb
+++ lib/virtuatable/builders/helpers/loaders.rb
@@ -2,12 +2,12 @@ module Virtuatable
   module Builders
     module Helpers
       module Loaders
-        extend ActiveSupport::Concern
-
         # @!attribute [rw] loaders
         #   @return [Array] an array of loading functions invoked at startup.
         attr_accessor :loaders
 
+        # Declares a loader in the current builder class.
+        # @param loader [Symbol] the name of the loader, infered as the method name to call.
         def declare_loader(loader)
           @loaders.nil? ? @loaders = [loader] : @loaders << loader
         end
