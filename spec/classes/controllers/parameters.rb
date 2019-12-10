dc76a38ea907d0b6ad6e8594ac59cc34ecb3fa3e
Vincent <courtois.vincent@outlook.com>
2019-12-10 13:41:19 +0100

    Refactore the params helper to keep the consistency with old versions

--- spec/classes/controllers/parameters.rb
+++ spec/classes/controllers/parameters.rb
@@ -1,13 +1,13 @@
 module Controllers
   class Parameters < Virtuatable::Controllers::Base
     post '/body/:id' do
-      halt 200, parameters.to_json
+      halt 200, params.to_json
     end
     get '/querystring/:id' do
-      halt 200, parameters.to_json
+      halt 200, params.to_json
     end
     post '/both/:id' do
-      halt 200, parameters.to_json
+      halt 200, params.to_json
     end
   end
 end
