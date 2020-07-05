eccf312f0f18e69a5457f5aa05d34d0231341760
Vincent Courtois <courtois.vincent@outlook.com>
2020-07-05 15:34:20 +0200

    Fix the docker command by re-adding the options

--- bin/commands/docker.rb
+++ bin/commands/docker.rb
@@ -7,8 +7,11 @@ module Commands
     end
 
     desc 'display <service>', 'Displays the content of the Dockerfile'
+    method_option :port, default: '8080'
+    method_option :bundler, default: '2.1.4'
+    method_option :ruby, default: '2.6.5'
     def display(service)
-      puts dockerfile(service).content
+      puts dockerfile(service, options).content
     end
 
     desc 'next <service>', 'Displays the next version of the service'
@@ -17,12 +20,17 @@ module Commands
     end
 
     desc 'create <service>', 'Creates a new image and pushes it'
+    method_option :port, default: '8080'
+    method_option :bundler, default: '2.1.4'
+    method_option :ruby, default: '2.6.5'
+    method_option :local, default: false, type: :boolean
     def create(service)
       v = Factories::Versions.instance.latest(service)
       if options[:local] == true
         system "docker build -t virtuatable/#{service}:#{v} ."
       else
-        system "docker build -t virtuatable/#{service}:#{v} -f #{dockerfile.store} ."
+        file = dockerfile(service, options).store
+        system "docker build -t virtuatable/#{service}:#{v} -f #{file} ."
       end
       system "docker push virtuatable/#{service}:#{v}"
     end
@@ -31,7 +39,7 @@ module Commands
 
     # Gets the representation of the dockerfile, with the variables interpolated.
     # @return [Virtuatable::Files::Dockerfile] the reprensetation of the dockerfile.
-    def dockerfile(service)
+    def dockerfile(service, options)
       Virtuatable::Files::Dockerfile.new(service, options)
     end
   end
